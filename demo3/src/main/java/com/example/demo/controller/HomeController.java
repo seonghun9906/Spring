82206686ff09f217587bbc94483af356216db36d
seonghun9906 <jsh990616@naver.com>
2024-01-12 15:56:13 +0900

    0112 spring

--- demo3/src/main/java/com/example/demo/controller/HomeController.java
+++ demo3/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-// @Controller : 해당 클래스를 컨트롤러 클래스로 스프링빈에 등록
-// 스프링빈 : 스프링이 관리해주는 자바 객체
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.dto.DemoDTO;
+
 import org.springframework.web.bind.annotation.PostMapping;
 
 
+
+
+// @Controller : 해당 클래스를 컨트롤러 클래스로 스프링빈에 등록
+// 스프링빈 : 스프링이 관리해주는 자바 객체
 @Controller //Controller 로 만드는것 , 어노테이션
 public class HomeController {
 	// / 주소에 대해 get 요청이 front 로 부터 오면 아래 메서드가 실행
@@ -60,6 +67,52 @@ public class HomeController {
 	}
 	
 	
+	@GetMapping("/model1")
+	public String model1(Model model) {
+		String str1 = "오늘은 금요일";
+		// addAttribute : 화면에 가져갈 데이터를 담음
+		model.addAttribute("s1",str1);
+		return "model1";
+	}
 	
+	@GetMapping("/form2")
+	public String form2() {
+		System.out.println("form2 메서드 호출");
+		return "form2";
+	}
+	
+	@PostMapping("/form2-param")
+	public String form2Param(@RequestParam("p1") String p1, Model model){
+		// 입력을 하고 전송버튼을 클릭하면 form-2param2라는 주소로 입력값을 전달한다.
+		model.addAttribute("p1",p1);
+		// 컨트롤러에서는 전달받은 값을 model에 담아서
+	System.out.println("p1 = " + p1);
+	return "model2";
+	// model2.html로 전달
+	}
+	
+	@GetMapping("/form3")
+	public String form3() {
+		return "form3";
+	
+	}
+
+	// public String postMethodName(@RequestParam("name") String name,@RequestParam("age") String age) {
+	//	System.out.println("name : " + name);
+	//	System.out.println("age : " + age);
+	//	System.out.println("name = " + name ", age = " + age);
+	//	return "model3";
+	
+	@PostMapping("/form-param3")
+	public String formparam3(@ModelAttribute DemoDTO demoDTO, Model model) {
+		System.out.println(demoDTO);
+		model.addAttribute("demo",demoDTO);
+		return "model3";
+	}
+
+		
 	
 }
+
+	
+
